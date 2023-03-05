87ff4bf05e0ee8ae48a96fdf9db9aa7430a79d7c
Ibrahim Mohamed <ibrahim.mohamed1218@gmail.com>
2023-03-05 09:32:47 +0000

    Create basic page layout

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div>
+      <h1>Header</h1>
+      <ul>
+        <li>Home</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Header;
