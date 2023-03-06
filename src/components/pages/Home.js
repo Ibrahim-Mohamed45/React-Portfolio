313b4a3a6308076a0f438e71d5095c743719cde1
Ibrahim Mohamed <ibrahim.mohamed1218@gmail.com>
2023-03-06 17:51:10 +0000

    Style home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,18 +1,19 @@
 import React from "react";
 import Avatar from "../../images/Ibrahim-Avatar.png";
 import Devices from "../../images/devices.png";
+import styles from "../../styles/Home.module.css"
 
 
 function Home() {
   return (
-    <div>
-      <div>
+    <div className={styles.homePage} >
+      <div className={styles.title} >
       <h1>Ibrahim Mohamed</h1>
       <p>Frontend Developer</p>
       <img src={Avatar} alt="Avatar of me" />
       </div>
-      <img src={Devices} alt="Pic of devices" />
-      <div>
+      <img src={Devices} className={styles.devices} alt="Pic of devices" />
+      <div className={styles.about}>
       <h2>Hi, I'm Ibrahim. Nice to meet you.</h2>
       <p>I'm an aspiring web developer who is intrested
         in all things Javascript. I am currently undertaking the EDx Trilogy Skills
