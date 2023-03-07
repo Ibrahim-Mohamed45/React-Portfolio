26630695604fa344db5f8653342ecd4ea1819b49
Ibrahim Mohamed <ibrahim.mohamed1218@gmail.com>
2023-03-07 14:21:07 +0000

    Style the navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,45 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "../styles/Header.module.css";
 
 function Header() {
   return (
-    <nav>
-    <ul>
-      <li>
-        <NavLink
-          to="/"
-          end
-        >
-          Home
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="projects"
-          end
-        >
-          Projects
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="contact"
-          end
-        >
-          Contact
-        </NavLink>
-      </li>
-    </ul>
+    <nav className={styles.navbar}>
+      <ul>
+        <li>
+          <NavLink
+            to="/"
+            end
+            className={({ isActive }) =>
+              isActive ? styles.active : styles.inactive
+            }
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="projects"
+            end
+            className={({ isActive }) =>
+              isActive ? styles.active : styles.inactive
+            }
+          >
+            Projects
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="contact"
+            end
+            className={({ isActive }) =>
+              isActive ? styles.active : styles.inactive
+            }
+          >
+            Contact
+          </NavLink>
+        </li>
+      </ul>
     </nav>
   );
 }
