9813e751a3a711eb80f830c783dec8be46425a56
Ibrahim Mohamed <ibrahim.mohamed1218@gmail.com>
2023-03-10 11:34:25 +0000

    Add contact form and style contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,10 +1,123 @@
-import React from 'react';
+import React from "react";
+import styles from "../../styles/Contact.module.css";
+import cv from "../../CV.pdf";
 
 function Contact() {
   return (
-    <div>
-      <h1>Contact Page</h1>
-    </div>
+    <section className={styles.contact}>
+      <div className={styles.padding}></div>
+      <a href={cv} download>
+        <button className={styles.btnCV}>
+          Download CV
+          <i
+            className="fa fa-download"
+            aria-hidden="true"
+            style={{ fontSize: "24px" }}
+          ></i>
+        </button>
+      </a>
+      <div className="row">
+        <div className={styles.display}>
+          <div className={styles.socials}>
+            <ul>
+              <li>
+                <a
+                  href="https://www.linkedin.com/in/ibrahim-mohamed-15ab4221a/"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  LinkedIn
+                  <i
+                    className="fa fa-linkedin-square"
+                    style={{ fontSize: "36px" }}
+                  ></i>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/Ibrahim-Mohamed45"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  GitHub
+                  <i className="fa fa-github" style={{ fontSize: "36px" }}></i>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="mailto:ibrahim.mohamed1218@gmail.com"
+                  target="_blank"
+                  className={styles.hidden}
+                  rel="noopener"
+                >
+                  Email me
+                  <i
+                    className="fa fa-envelope"
+                    style={{ fontSize: "36px" }}
+                  ></i>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="tel:+447939809870"
+                  className={styles.hidden}
+                  target="_blank"
+                  rel="noopener"
+                >
+                  Call me
+                  <i className="fa fa-phone" style={{ fontSize: "36px" }}></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-lg-6 col-sm-12">
+          <div className={styles.formDiv}>
+            <form
+              action="https://formsubmit.co/ibrahim.mohamed1218@gmail.com"
+              method="POST"
+            >
+              <h3>GET IN TOUCH</h3>
+              <input type="hidden" name="_subject" value="New submission!" />
+              <input type="text" name="name" placeholder="Name" required />
+              <input type="email" name="email" placeholder="Email" required />
+              <textarea
+                name="message"
+                rows="5"
+                cols="60"
+                placeholder="Your message"
+              ></textarea>
+              <button type="submit">Send</button>
+            </form>
+          </div>
+        </div>
+        <div className={styles.display}>
+          <div className={styles.socials}>
+            <ul className={styles.contactMe}>
+              <li>
+                <a
+                  href="mailto:ibrahim.mohamed1218@gmail.com"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  Email me
+                  <i
+                    className="fa fa-envelope"
+                    style={{ fontSize: "36px" }}
+                  ></i>
+                </a>
+              </li>
+              <li>
+                <a href="tel:+447939809870" target="_blank" rel="noopener">
+                  Call me
+                  <i className="fa fa-phone" style={{ fontSize: "36px" }}></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
