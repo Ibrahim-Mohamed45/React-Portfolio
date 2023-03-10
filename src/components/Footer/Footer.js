dc6206e4935148826a846d9545a8c725e7d96c20
Ibrahim Mohamed <ibrahim.mohamed1218@gmail.com>
2023-03-10 18:28:32 +0000

    Add footer to the page

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,24 @@
 import React from "react";
 
 function Footer() {
+  const year = new Date().getFullYear();
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer className="footer">
+      <p>
+        Copyright © {year} Ibrahim Mohamed
+        <a
+          href="https://github.com/Ibrahim-Mohamed45"
+          target="_blank"
+          rel="noreferrer"
+          className="test"
+        >
+          <i
+            className="fa fa-github github"
+            style={{ color: "black", fontSize: "26px" }}
+          ></i>
+        </a>
+      </p>
+    </footer>
   );
 }
 
